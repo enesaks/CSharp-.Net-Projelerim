2081163c5380347a73ffae64e3b9d4ffb1af7473
Enes <98012557+enesaks@users.noreply.github.com>
2024-03-13 02:15:06 +0300

    Portfolio Project Kısmı Oluşturulduç

---
+++ StellerAcunMedyaAkedemiPortfolio/StellerAcunMedyaAkedemiPortfolio/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StellerAcunMedyaAkedemiPortfolio.Models;
+
+namespace StellerAcunMedyaAkedemiPortfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+
+        StellerAcunMedyaDbEntities db = new StellerAcunMedyaDbEntities();
+
+        public ActionResult Index()
+        {
+
+            var projectList = db.TblProject.ToList();
+              
+            return View(projectList);
+        }
+    }
+}
