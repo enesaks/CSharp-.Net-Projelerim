0db4256759156e5c95b62d1b9d5aaf1eed495039
Enes <98012557+enesaks@users.noreply.github.com>
2024-04-29 22:11:27 +0300

    Steller Portfolio Uı kısmı bitti

---
+++ StellerPortfolio/StellerPortfolio/Controllers/UILayoutController.cs
@@ -0,0 +1,21 @@
+﻿using StellerPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StellerPortfolio.Controllers
+{
+    [AllowAnonymous]
+    public class UILayoutController : Controller
+    {
+        // GET: UILayout
+        StellerAcunMedyaDbEntities2 db = new StellerAcunMedyaDbEntities2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
